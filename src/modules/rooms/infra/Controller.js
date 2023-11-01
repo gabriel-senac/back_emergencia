8464f3a81e46132d88df54c4e33e53e11a16126a
Mãos Leves <wilton_siq@hotmail.com>
2023-11-01 19:53:04 -0300

    Criação da função put (atualização)

--- src/modules/rooms/infra/Controller.js
+++ src/modules/rooms/infra/Controller.js
@@ -18,7 +18,18 @@ export class Controller {
         const {room} = request.body;
         try {
             const [rows, fields] = await connection.query('INSERT INTO rooms WHERE room = ?', [room]);
-            return response.json(rows);
+            return response.status(200).json(rows);
+            } catch (error) {
+                console.error(error);
+                return response.status(500).json({ message: 'Não foi possível identificar as salas.' });
+            }
+    }
+   
+    async putUpdate(request, response) {
+        const {room} = request.body
+        try {
+            const [rows, fields] = await connection.query('Emergency call on', [room]);
+            return response.status(200).json(rows);
             } catch (error) {
                 console.error(error);
                 return response.status(500).json({ message: 'Não foi possível identificar as salas.' });
@@ -27,5 +38,6 @@ export class Controller {
   
 
 }
+
 
 
--- src/modules/rooms/infra/routes.js
+++ src/modules/rooms/infra/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { Controller } from './Controller.js';
 import { Solicitacao } from './Controller.js';
+import { Update } from './Controller.js';
 
 export const usersRouter = Router();
 const controller = new Controller();
@@ -8,5 +9,5 @@ const controller = new Controller();
 
 usersRouter.get('/selecionarSalas', controller.getRooms)
 usersRouter.post('/sendEmergency', controller.postRequerimento)
-
+usersRouter.put('/sendUpdate', controller.putUpdate)
 
