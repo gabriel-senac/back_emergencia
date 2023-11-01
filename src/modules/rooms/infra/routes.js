0fd4156a26506a2cdbc7681ec268dd7042ae9dfb
Barriguinha Saliente <lucas.marques.sud@gmail.com>
2023-10-31 21:15:17 -0300

    Requisição da sala de emergência vinda do desktop

--- src/modules/rooms/infra/routes.js
+++ src/modules/rooms/infra/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { Controller } from './Controller.js';
+import { Solicitacao } from './Controller.js';
 
 export const usersRouter = Router();
 const controller = new Controller();
 
-usersRouter.get('/selecionarSalas', controller.getRooms)
 
+usersRouter.get('/selecionarSalas', controller.getRooms)
+usersRouter.post('/sendEmergency', controller.postRequerimento)
 
 
