67f1f878cdbb53201da7bb7ac96f3d489326471c
GABRIEL BARRETO <gabrielbarreto_user@hotmail.com>
2023-11-06 19:15:01 -0300

    ajustes nas validações da api

--- src/modules/rooms/infra/Controller.js
+++ src/modules/rooms/infra/Controller.js
@@ -9,41 +9,38 @@ export class Controller {
             return response.status(200).json(rows);
             } catch (error) {
                 console.error(error);
-                return response.status(500).json({ message: 'Não foi possível identificar as salas.' });
+                return response.status(500).json({ message: 'Não foi possível obter as salas.' });
             }
     }
   
 
     async postRequerimento(request, response) {
         const {room} = request.body;
+        if(room ===undefined || room === null ||  room === ''){
+            return response.status(400).json({ message: 'Sala indefinida.' });
+        }
         try {
             const [rows, fields] = await connection.query('INSERT INTO rooms WHERE room = ?', [room]);
-            return response.status(200).json(rows);
+            return response.status(201).json(rows);
             } catch (error) {
                 console.error(error);
-                return response.status(500).json({ message: 'Não foi possível identificar as salas.' });
+                return response.status(500).json({ message: 'Não foi possivel criar emergencia da sala.' });
             }
     }
    
     async putUpdate(request, response) {
         const {room} = request.body
+
+        if(room ===undefined || room === null ||  room === ''){
+            return response.status(400).json({ message: 'Sala indefinida.' });
+        }
+
         try {
             const [rows, fields] = await connection.query('Emergency call on', [room]);
-            return response.status(200).json(rows);
+            return response.status(200).json({message: "Emergencia da sala atualizada"});
             } catch (error) {
                 console.error(error);
-                return response.status(500).json({ message: 'Não foi possível identificar as salas.' });
-            }
-    }
-    
-    async statusError(request, response) {
-        const {room} = request.body
-        try {
-            const [rows, fields] = await connection.query('/');
-            return response.status(400).json(rows);
-            } catch (error) {
-                console.error(error);
-                return response.status(500).json({ message: 'Não foi possível identificar as salas.' });
+                return response.status(500).json({ message: 'Não foi possível atualizar a emergencia da sala.' });
             }
     }
   
