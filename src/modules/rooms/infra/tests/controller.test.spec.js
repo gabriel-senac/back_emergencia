1189de561e3f1548cf45896860f2167c8c00ce3c
GABRIEL BARRETO <gabrielbarreto_user@hotmail.com>
2024-01-22 21:22:13 -0300

    testes unitarios back end

---
+++ src/modules/rooms/infra/tests/controller.test.spec.js
@@ -0,0 +1,68 @@
+import { Controller } from "../Controller";
+
+import { connection } from "../../../../connection"
+
+jest.mock('../../../../connection')
+
+//DESCRIBE - 
+//IT 
+//EXPECT
+
+describe('Controller', () => {
+
+    let controller;
+
+    beforeEach(() => {
+        controller = new Controller();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('getRoomsA', () => {
+        it('should get rooms from blocoA', async () => {
+            const mockRows = [{ sala: 'Sala A1' }];
+
+            connection.query.mockResolvedValueOnce([mockRows, null])
+
+            const mockRequest = {};
+            const mockResponse = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            }
+
+            await controller.getRoomsA(mockRequest, mockResponse);
+
+            expect(connection.query).toHaveBeenCalledWith('SELECT * FROM blocoA');
+            expect(mockResponse.status).toHaveBeenCalledWith(200);
+            expect(mockResponse.json).toHaveBeenCalledWith([mockRows]);
+        });
+
+        // it('should handle error and return 500 status', async () => {
+
+        //     const mockError = new Error('Database Error');
+
+        //     connection.query.mockResolvedValueOnce([mockError, null])
+
+        //     const mockRequest = {};
+        //     const mockResponse = {
+        //         status: jest.fn().mockReturnThis(),
+        //         json: jest.fn()
+        //     }
+
+        //     await controller.getRoomsA(mockRequest, mockResponse);
+
+        //     expect(connection.query).toHaveBeenCalledWith('SELECT * FROM blocoA');
+        //     expect(mockResponse.status).toHaveBeenCalledWith(500);
+        //     expect(mockResponse.json).toHaveBeenCalledWith({
+        //         message: 'Não foi possivel obter as salas'
+        //     });
+
+
+        // });
+
+    });
+
+});
+
